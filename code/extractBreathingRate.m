5f7415a6feb7efe378d5edfdeb2325c98f1d54d3
Akhil Polamarasetty <akhil_polamarasetty@Akhils-MacBook-Pro.local>
2021-12-16 21:49:22 -0600

    Common rate estimates for gt and csi

--- code/extractBreathingRate.m
+++ code/extractBreathingRate.m
@@ -1,57 +1,67 @@
-
-ground_truth = load('data/data_15/ground_truth_12_11_2021_5min_15_100ms.mat');
-force = ground_truth.force';
-time_signal = ground_truth.curr_time';
-
-start_time = time_signal(1);
-time_diff_signal = time_signal - start_time;
-time_diff_signal = time_diff_signal;
-
-% apply plomb only to a window of 10s
-start_ix = 1;
-next_step_found = false;
-next_start_ix = 1;
-window_ix = start_ix + 1; 
-
-breathing_ground = [];
-timestamps = [];
-
-N = size(force, 1);
-R_bound = [0.1 0.6];
-
-while window_ix < N
-    time_diff = time_diff_signal(window_ix) - time_diff_signal(start_ix);
-    if (time_diff > 15)
-        fprintf("%0.5f | %d | %d\n", time_diff, window_ix, start_ix);
-        time_window = time_diff_signal(start_ix:window_ix);
-        signal_window = force(start_ix:window_ix,:);
-        [pxx, f] = plomb(signal_window, time_window);
-        idx = find(f >= R_bound(1) & f <= R_bound(2));
-        f = f(idx);
-        pxx = pxx(idx);
-        [freqPeak, loc] = max(pxx);
-        
-        breathing_ground = [breathing_ground; f(loc)];
-        timestamps = [timestamps; [time_signal(start_ix) time_signal(window_ix)]];
-        if (next_step_found)
-            start_ix = next_start_ix;
-            window_ix = start_ix + 1;
-            next_step_found = false;
-        else
-            start_ix = window_ix;
-            window_ix = start_ix + 1;            
-        end
-    elseif (time_diff > 1 && not(next_step_found))
-        fprintf("%0.5f | %d | %d\n", time_diff, next_start_ix, start_ix);
-        next_start_ix = window_ix;
-        next_step_found = true;
-    else
-        window_ix = window_ix +1;
+for k = 10:12
+    num = 5;
+    if (k >= 13)
+        num = 11;
     end
-end
+    ground_truth_file = ['data/data_' num2str(k) '/ground_truth_12_' num2str(num) '_2021_5min_' num2str(k) '_100ms.mat'];
+    ground_truth = load(ground_truth_file);
+    force = ground_truth.force';
+    veriner_estimates = ground_truth.RR_bpm;
+    time_signal = ground_truth.curr_time';
 
+    start_time = time_signal(1); 
+    time_diff_signal = time_signal - start_time;
 
-breathing_ground = breathing_ground*60;
-plot(breathing_ground)
-save('data/data_15/breathing_ground.mat',"breathing_ground");
-save('data/data_15/timestamps_ground.mat',"timestamps");
+    % apply plomb only to a window of 10s
+    start_ix = 1;
+    next_step_found = false;
+    next_start_ix = 1;
+    window_ix = start_ix + 1; 
+
+    breathing_ground = [];
+    timestamps = [];
+
+    N = size(force, 1);
+    R_bound = [0.1 0.6];
+
+    while window_ix < N
+      time_diff = time_diff_signal(window_ix) - time_diff_signal(start_ix);
+      if (time_diff > 30)
+          %fprintf("%0.5f | %d | %d\n", time_diff, window_ix, start_ix);
+          time_window = time_signal(start_ix:window_ix);
+          signal_window = force(start_ix:window_ix,:);
+          [pxx, f] = plomb(signal_window, time_window);
+          idx = find(f >= R_bound(1) & f <= R_bound(2));
+          f = f(idx);
+          pxx = pxx(idx);
+          [freqPeak, loc] = max(pxx);
+        
+          breathing_ground = [breathing_ground; f(loc)];
+          timestamps = [timestamps; [time_signal(start_ix) time_signal(window_ix)]];
+          if (next_step_found)
+              start_ix = next_start_ix;
+              window_ix = start_ix + 1;
+              next_step_found = false;
+          else
+              start_ix = window_ix;
+              window_ix = start_ix + 1;            
+          end
+      elseif (time_diff > 1 && not(next_step_found))
+          %fprintf("%0.5f | %d | %d\n", time_diff, next_start_ix, start_ix);
+          next_start_ix = window_ix;
+          next_step_found = true;
+      else
+          window_ix = window_ix +1;
+      end
+    end
+    breathing_ground = breathing_ground*60;
+    figure;
+    plot(timestamps(:,2), breathing_ground);
+    hold on;
+    plot(time_signal(1:100:end), veriner_estimates(1:100:end));
+    filename = ['data/data_' num2str(k) '/breathing_ground_ts.mat'];
+    save(filename,"breathing_ground");
+    filename = ['data/data_' num2str(k) '/timestamps_ground_ts.mat'];
+    save(filename,"timestamps");
+    fprintf("Completed the processing of gt for dataset-%d\n",k);
+end   
--- code/new_br_estimates.m
+++ code/new_br_estimates.m
@@ -1,6 +1,6 @@
-%{
 base_dir = 'data/data_';
-for k = 4:15
+%{
+for k = 7:9
     input_filePath = [base_dir num2str(k) '/csi_signal.mat'];
     csi_signal_mat = load(input_filePath, 'stream1');
     stream = csi_signal_mat.stream1;
@@ -14,7 +14,7 @@ end
 
 fprintf("All PD Signals generated ..... \n");
 fprintf("Generating breathing rate estimates ..... \n");
-for k = 4:15
+for k = 7:9
     input_filePath = [base_dir num2str(k) '/new_pd_signal.mat'];
     % load the phase difference data of all spatial streams
     pd_signal_mat = load(input_filePath, 'pd_signal');
@@ -27,15 +27,14 @@ for k = 4:15
     time_diff_signal = time_diff_signal * 0.001;
     N = time_diff_signal(end);
 
-    % apply bandpass & plomb only to a window of 15s
-    step = 1; window = 15; % in seconds
-    start_ix = 0; finish_ix = start_ix + window;
-    br_estimates = []; br_estimates_all = []; timestamps = [];
+    % apply bandpass & plomb only to a window of 15s 
+    % all time signals are in seconds
+    step = 1; window = 15; start_ix = 0; finish_ix = start_ix + window; br_estimates = []; br_estimates_all = []; timestamps = [];
     % windowing
     while finish_ix <= N
         index = find(time_diff_signal >= start_ix & time_diff_signal <= finish_ix);
         if isempty(index)
-            fprintf('dataset:%d | [%d %d] is empty\n', k, start_ix, finish_ix);
+            fprintf('dataset:%d | CSI | [%d %d] is empty\n', k, start_ix, finish_ix);
             start_ix = start_ix + step;
             finish_ix = start_ix + window;
             continue
@@ -44,25 +43,80 @@ for k = 4:15
         windowed_signal = pd_signal(index, :);
         % filter the breathing frequencies using bandpass filter
         filtered_signal = bandpass(windowed_signal, [0.1 0.6], 40);
-        %plot(filtered_signal(:,20));
+        %plot(window_time, filtered_signal);
         curr_window_br_estimate = get_respiration_rates(filtered_signal, window_time);
         if isempty(curr_window_br_estimate)
-            fprintf('dataset:%d | [%d %d] [%.9f %.9f] br_estimates are empty\n', k, start_ix, finish_ix, window_time(1), window_time(end));
+            fprintf('dataset:%d | CSI | [%d %d] [%.9f %.9f] br_estimates are empty\n', k, start_ix, finish_ix, window_time(1), window_time(end));
             start_ix = start_ix + step;
             finish_ix = start_ix + window;            
             continue        
         end
         br_estimates = [br_estimates; curr_window_br_estimate'];
-        timestamps = [timestamps; [window_time(1)*1000 window_time(end)]*1000];
-        fprintf('dataset:%d | [%d %d] [%.9f %.9f] is processed\n', k, start_ix, finish_ix, window_time(1), window_time(end));
+        timestamps = [timestamps; [window_time(1)*1000 window_time(end)*1000]];
+        fprintf('dataset:%d | CSI | [%d %d] [%.9f %.9f] is processed\n', k, start_ix, finish_ix, window_time(1), window_time(end));
         start_ix = start_ix + step;
         finish_ix = start_ix + window;
     end
-    plot(mean(br_estimates,2));
     %output_filePath = [base_dir num2str(k) '/new_br_estimates_plomb.fig'];
     %saveas(gcf,output_filePath);
     output_filePath = [base_dir num2str(k) '/new_br_estimates_plomb.mat'];
     save(output_filePath,"br_estimates");
     output_filePath = [base_dir num2str(k) '/new_br_timestamps_plomb.mat'];
     save(output_filePath,"timestamps");
+
+    num = 5;
+    if (k >= 13)
+        num = 11;
+    end
+    ground_truth_file = ['data/data_' num2str(k) '/ground_truth_12_' num2str(num) '_2021_5min_' num2str(k) '_100ms.mat'];
+    ground_truth = load(ground_truth_file);
+    force = ground_truth.force';
+    veriner_estimates = ground_truth.RR_bpm;
+    time_signal = ground_truth.curr_time';
+
+    start_time = time_signal(1); 
+    time_diff_signal = time_signal - start_time;
+    N = time_diff_signal(end);
+
+    % apply plomb only to a window of 10s
+    start_ix = 0; finish_ix = start_ix + window; breathing_ground = []; timestamps_gt = []; R_bound = [0.1 0.6];
+    % windowing
+    while finish_ix <= N
+        index = find(time_diff_signal >= start_ix & time_diff_signal <= finish_ix);
+        if isempty(index)
+            fprintf('dataset:%d | GT | [%d %d] is empty\n', k, start_ix, finish_ix);
+            start_ix = start_ix + step;
+            finish_ix = start_ix + window;
+            continue
+        end
+        window_time = time_signal(index);
+        windowed_signal = force(index);
+        % filter the breathing frequencies using bandpass filter - force
+        % readings are out every 100ms so frequency is 10Hz 
+        %filtered_signal = bandpass(windowed_signal, [0.1 0.6], 10);
+        curr_window_br_estimate = get_respiration_rates(windowed_signal, window_time);
+        if isempty(curr_window_br_estimate)
+            fprintf('dataset:%d | GT | [%d %d] [%.9f %.9f] br_estimates are empty\n', k, start_ix, finish_ix, window_time(1), window_time(end));
+            start_ix = start_ix + step;
+            finish_ix = start_ix + window;            
+            continue        
+        end
+        breathing_ground = [breathing_ground; curr_window_br_estimate'];
+        timestamps_gt = [timestamps_gt; [window_time(1) window_time(end)]];
+        fprintf('dataset:%d | GT | [%d %d] [%.9f %.9f] is processed\n', k, start_ix, finish_ix, window_time(1), window_time(end));
+        start_ix = start_ix + step;
+        finish_ix = start_ix + window;
+    end
+    filename = ['data/data_' num2str(k) '/breathing_ground_ts.mat'];
+    save(filename,"breathing_ground");
+    filename = ['data/data_' num2str(k) '/timestamps_ground_ts.mat'];
+    save(filename,"timestamps_gt");
+    fprintf("Completed the processing of gt for dataset-%d\n",k);
+
+    figure;
+    plot(breathing_ground);
+    hold on;
+    plot(veriner_estimates(1:100:end));
+    hold on;
+    plot(br_estimates(:,20));
 end
